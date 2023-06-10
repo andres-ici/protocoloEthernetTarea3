eb8d25b7a6abbd31d2421708fc6cd7d9d1c6ce26
José A. Mena <pepemena1@gmail.com>
2023-06-10 01:00:58 -0400

    Se logra extraer los datos de los txt y se modificaron los nodos_X.txt

--- anillo.cpp
+++ anillo.cpp
@@ -7,10 +7,15 @@
 //Functions
 
 //void readFromFile();
-void readFile(const char* filePath);
+void readFile(const char* filePath, char* variables[4]);
+
+
+
+char* txt[4];
 
 int main(int argc, char* argv[]) {
     // Print the number of command line arguments entered
+
     printf("You have entered %d arguments:\n", argc);
 
     // Print each command line argument entered
@@ -31,18 +36,26 @@ int main(int argc, char* argv[]) {
     // Determine which node the program is running on
     if (strcmp(argv[1], "1") == 0) {
         printf("Nodo 1\n");
+        const char* filePath = "nodo_1.txt"; 
+        readFile(filePath,txt);
     } else if (strcmp(argv[1], "2") == 0) {
         printf("Nodo 2\n");
+        const char* filePath = "nodo_2.txt"; 
+        readFile(filePath,txt);
     } else if (strcmp(argv[1], "3") == 0) {
         printf("Nodo 3\n");
+        const char* filePath = "nodo_3.txt"; 
+        readFile(filePath,txt);
     } else {
         // Print an error message if the node is not recognized
         printf("Error con el Nodo\n");
     }
 
-
-    const char* filePath = "nodo_1.txt"; // Replace "example.txt" with your file path
-    readFile(filePath);
+    printf("M.A.C: %s\n",txt[0]);
+    printf("PinIn: %s\n",txt[1]);
+    printf("PintOut: %s\n",txt[2]);
+    printf("Clock: %s\n",txt[3]);
+ 
 
 
     // Return 0 to indicate successful program execution
@@ -79,7 +92,8 @@ int main(int argc, char* argv[]) {
 // }
 
 
-void readFile(const char* filePath) {
+void readFile(const char* filePath, char* variables[4]) {
+    int lineNumber = 1; // Start from line 1
     FILE* inputFile = fopen(filePath, "r");
     if (inputFile == nullptr) {
         printf("Failed to open the file.\n");
@@ -87,8 +101,17 @@ void readFile(const char* filePath) {
     }
 
     char line[256];
+    int x = 0;
     while (fgets(line, sizeof(line), inputFile) != nullptr) {
-        printf("%s", line);
+        // Check if the current line number matches the desired lines
+        printf("line(%d): %s",lineNumber, line);
+        if (lineNumber == 2 || lineNumber == 5 || lineNumber == 8 || lineNumber == 11) {
+            // Make a copy of the line and assign it to the corresponding variable
+            variables[x] = strdup(line);
+            //printf("line#: %d, valor: %d\n",lineNumber, x);
+            x++;
+        }
+        lineNumber++;
     }
 
     fclose(inputFile);
