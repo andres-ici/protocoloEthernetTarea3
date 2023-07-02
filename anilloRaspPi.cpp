db3b4fa9acc38b14cbb7bec184199590a92e7b33
Andres C <a.carrasco12@ufromail.cl>
2023-07-01 23:16:24 -0400

    Se agrego version final con problemas en el SLIP del receptor

--- anilloRaspPi.cpp
+++ anilloRaspPi.cpp
@@ -26,6 +26,11 @@ void generateFrameSLIP(BYTE* frame, BYTE* frameSLIP, int lenghtFrame);
 void startTransmission();
 void sendFrame(void);
 
+//Funciones de recepcion de datos
+
+void leerBit();
+bool compararMAC(BYTE MACdestino[6],const char *MAC);
+
 //Variables
 
 bool emisor;
@@ -33,10 +38,28 @@ int nodo;
 char* varTxt[4];
 int largoSlipProtocol;
 int PinOut;
+int PinIn;
 
 BYTE slipProtocol[100];
 volatile int nbits = 0; //Cuenta la cantidad de bits recibidos
 volatile int nbytes = 0; //Cuenta el número de Byte enviados
+
+volatile int j = 0; //Cuenta la cantidad de bits hasta formar un byte
+BYTE c = 0; //Se forma y se guarda el byte actual recibido
+BYTE d = 0; //Representa el valor previo de “c”
+bool transmissionStarted2 = false; //Indica el estado de la transmisión 
+bool finished = false; //Finaliza el programa 
+
+BYTE C0 = 192; //Guarda el valor de decimal de C0 en C0
+BYTE DB = 219; //Guarda el valor de decimal de DB en DB
+BYTE DC = 220; //Guarda el valor de decimal de DC en DC
+BYTE DD = 221; //Guarda el valor de decimal de DD en DD
+
+BYTE msg[300]; //Se guarda todo el mensaje recibido excepto los bytes de inicio y final 
+//int index = 0; //Desface requerido para guardar el byte en caso de encontrar un DBDC y/o DBDD
+int B = 0; //Cantidad de bytes recibidos
+int FCScalculadoCapaEthernet = 0; //Se guarda el valor del FCS calculada del receptor
+int FCScalculadoCapaPropio = 0; //Se guarda el valor del FCS calculada del receptor
 
 //Variables de envio de datos
 
@@ -106,7 +129,7 @@ int main(int argc, char* argv[]){
     }
 
     char *MAC = varTxt[0]; 
-    int PinIn = atoi(varTxt[1]);
+    PinIn = atoi(varTxt[1]);
     PinOut = atoi(varTxt[2]);
     int Clock = atoi(varTxt[3]);
 
@@ -242,7 +265,118 @@ int main(int argc, char* argv[]){
 
     }
 
+    //Codigo para el Receptor 
 
+    pinMode(PinIn, INPUT);
+    printf("Esperando...\n");
+
+    if(wiringPiISR(Clock, INT_EDGE_RISING, &leerBit) < 0){ //Interrumpe el programa para leer el mensaje
+    printf("Unable to start interrupt function\n");
+    }
+
+    while(!finished); 
+    printf("\n");
+
+    BYTE MACdestino[6] = {msg[0], msg[1], msg[2], msg[3], msg[4], msg[5]};
+    BYTE MACorigen[6] = {msg[6], msg[7], msg[8], msg[9], msg[10], msg[11]};
+
+    printf("MACdestino: %02X:%02X:%02X:%02X:%02X:%02X\n", msg[0], msg[1], msg[2], msg[3], msg[4], msg[5]);
+    printf("MACorigen: %02X:%02X:%02X:%02X:%02X:%02X\n", msg[6], msg[7], msg[8], msg[9], msg[10], msg[11]);
+
+    if(compararMAC(MACdestino, MAC)){ //Compara si la MAC del destino es igual a la MAC del receptor
+        
+        int largoCapaEthernet = msg[13] + (msg[14] << 8);
+        int FCScapaEthernet = 0;
+        int x = 0;
+        for(int i = (16 + largoCapaEthernet); i <= (16 + largoCapaEthernet) + 4; i++){
+        
+            FCScapaEthernet =  FCScapaEthernet + (msg[i] << x*8);
+            x++;
+        }
+        
+        for(int i = 0; i <= (15 + largoCapaEthernet); i++){ //Calcula el FCS 
+            for(int x = 0; x < 8; x++){
+                FCScalculadoCapaEthernet = FCScalculadoCapaEthernet + (int)((msg[i] >> x) & 0x01);
+            }
+        }
+
+
+       
+        // Data + Relleno: [15, 15 + Longitud]
+
+        // msg[15]       = |L|L|L|L|L|C|C|C|
+        // msg[16 al 46] = |D|D|D|D|D|D|D|D|
+        // msg[47]       = |F|F|F|F|F|F|F|F|
+        // msg[48]       = |_|_|_|_|_|_|_|F|
+
+
+        // Destribucion Capa Propia
+        //CMD: (msg[15] & 0x07) 
+        //Longitud: (msg[15] & 0xF8) >> 3
+        //Dato: msg[15] ... msg[15 + longitud]
+        //FCS: msg[16 + longitud] | (msg[17 + longitud] & 0x01) << 8 
+
+
+        int cmdCapaPropio = msg[15] & 0x07;
+        int largoCapaPropio = (msg[15] & 0xF8) >> 3;
+        int FCScapaPropio = msg[16 + largoCapaPropio] | (msg[17 + largoCapaPropio] & 0x01) << 8;
+
+
+        for(int i = 15; i <= (15 + largoCapaPropio); i++){ //Calcula el FCS 
+            for(int x = 0; x < 8; x++){
+                FCScalculadoCapaPropio = FCScalculadoCapaPropio + (int)((msg[i] >> x) & 0x01);
+            }
+        }
+       
+
+
+
+        printf("\n\nCapa_Ethernet\n");
+        printf("M.A.C Destino:");
+        for(int i = 0 ; i <= 5; i++){
+            printf("%c",msg[i]);
+        }
+        printf("\n");
+        printf("M.A.C Origen:");
+        for(int i = 6 ; i <= 11; i++){
+            printf("%c",msg[i]);
+        }
+        printf("\n");
+        printf("TTL:%d\n",msg[12]);
+        printf("largoCapaEthernet: %d\n",largoCapaEthernet);
+        printf("FCScapaEthernet: %d\n",FCScapaEthernet);
+        printf("FCScalculadoCapaEthernet: %d\n",FCScalculadoCapaEthernet);
+
+
+        printf("\n\nCapa_Propia\n");
+        printf("cmdCapaPropia: %d\n",cmdCapaPropio);
+        printf("largoCapaPropia: %d\n",largoCapaPropio);
+        printf("FCScapaPropio: %d\n",FCScapaPropio);
+        printf("FCScalculadoCapaPropio: %d\n",FCScalculadoCapaPropio);
+
+        printf("\n\nMensaje_Recibido\n");
+        for(int i = 16; i < (16 + largoCapaPropio);i++){
+            printf("%c",msg[i]);
+        }
+        printf("\n\n\n\n");
+
+
+
+    }else{
+        msg[12] = msg[12] - 1; //Resta al TTL
+
+        if(msg[12] == 0){ //No se renvia el mensaje 
+        printf("Mensaje descartado por TTL\n");
+        printf("TTL:%d\n",msg[12]);
+
+        }else{ //Se renvia el mensaje
+        printf("Reenviando...\n");
+        printf("TTL:%d\n",msg[12]);
+
+        }
+
+
+    }
 
     return 0;
 
@@ -484,4 +618,93 @@ void startTransmission(){   //Cambia el condicional que controla la transmisión
 
     transmissionStarted = true;
 
+}
+
+void leerBit(){
+    int largo; //Se inicializa el largo del mensaje 
+    int cmd; //Se inicializa el comando del mensaje
+    int comienzoData = 1; //Se inicializa el comienzo de la data
+    int MACdestino = 0;
+    bool bit = digitalRead(PinIn); //Se leen los bits del pin
+    int index = 0;
+  
+    c = (c << 1) | bit; //Se guardan los bits en “c”
+
+    if(transmissionStarted2 == false){
+        if(c == C0){ //Si se detecta un C0 se inicia la transmisión y se resetean las variables
+            printf("inicio\n");
+            j = 0;
+            B = 0;
+            c = 0;
+            d = 0;  
+            index = 0;
+            transmissionStarted2 = true;
+        }
+    }else{
+        printf ("%d", bit);
+        j++; 
+
+        if(j == 8){
+            j = 0;   
+
+            msg[B-index] = c; //Se guarda el byte de “c” en msg[] 
+
+            if(c == C0){ //Se activa con el segundo C0 es decir con el byte de final
+                finished = true;
+                for(int i = 0; i < 100; i++){
+            
+                    printf("MSG[%i]: %i\n", i, msg[i]);
+
+                }
+                return;
+            }else{
+                printf("  %d  %c\n",c,c);
+            }
+
+
+            if(d == DB & c == DC){ //Detecta DBDC
+                printf("Se Recibio un: DBDC\n");
+                
+                msg[B-index] = 0; //Transforma el DB en 0 
+                index++;
+                msg[B-index] = C0; //Transforma el DC en C0
+            }  
+            if(d == DB & c == DD){ //Detecta DBDD
+                printf("Se Recibio un: DBDD\n");
+
+                msg[B-index] = 0; //Transforma el DB en 0
+                index++;
+                msg[B-index] = DB; //Transforma el DD en DB
+            } 
+
+            d = c;  
+            B++;
+        }
+    }
+
+}
+
+bool compararMAC(BYTE MACdestino[6],const char *MAC){
+
+    BYTE MACconvertido[6];
+    converterMAC(MAC,MACconvertido);
+
+    printf("Mac Convertido\n");
+    for(int i = 0; i < 6 ; i++){
+        printf("%d",MACconvertido[i]);
+    }
+    printf("\n");
+
+    int contador = 0;
+
+    for(int i = 0; i < 6; i++){
+        if(MACconvertido[i] == MACdestino[i]){
+            contador++;
+        }
+    }
+    if(contador == 6){
+        return true;
+    }
+
+    return false;
 }
